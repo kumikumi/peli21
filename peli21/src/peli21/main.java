5819dd9d642e989cfcd023a01594b94c5af0ded5
Mikko Kumara <mikko.kumara@gmail.com>
2013-01-22 18:16:00 +0200

    Added packages peli, domain. Added some initial classes and tests.

--- peli21/src/peli21/main.java
+++ peli21/src/peli21/main.java
@@ -4,6 +4,9 @@
  */
 package peli21;
 
+import peli21.domain.Ruudukko;
+import peli21.domain.Ruutu;
+
 /**
  *
  * @author mikko
@@ -15,6 +18,26 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hei maailma!");
+        System.out.println("Seisotte nyt paikalla, johon on valmistumassa eeppinen ruudukkopeli.");
+//        
+//        System.out.println("Tehtiin ruudukko mitoilla (5, 4)");
+//        Ruudukko ruudukko1 = new Ruudukko(5, 4);
+//        
+//        ruudukko1.setRuutu(2, 3, new Ruutu(true, true, false, false));
+//        ruudukko1.getRuutu(3, 3).setDown(true);
+//        
+//        Ruutu ruutu1 = ruudukko1.getRuutu(2, 3);
+//        System.out.println("Ruudukon ruutu (2,3): ");
+//        System.out.println("UP: " + ruutu1.isUp());
+//        System.out.println("DOWN: " + ruutu1.isDown());
+//        System.out.println("LEFT: " + ruutu1.isLeft());
+//        System.out.println("RIGHT: " + ruutu1.isRight());
+//        
+//        Ruutu ruutu2 = ruudukko1.getRuutu(3, 3);
+//        System.out.println("Ruudukon ruutu (3,3): ");
+//        System.out.println("UP: " + ruutu2.isUp());
+//        System.out.println("DOWN: " + ruutu2.isDown());
+//        System.out.println("LEFT: " + ruutu2.isLeft());
+//        System.out.println("RIGHT: " + ruutu2.isRight());
     }
 }
