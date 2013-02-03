cd4b7bc03db32da29cb47ba5b0beffc0de3d5d6a
Mikko Kumara <mikko.kumara@gmail.com>
2013-02-04 01:43:16 +0200

    Added highscores.

--- peli21/src/peli21/main.java
+++ peli21/src/peli21/main.java
@@ -4,10 +4,12 @@
  */
 package peli21;
 
+import java.io.File;
 import javax.swing.SwingUtilities;
 import peli21.domain.Ruudukko;
 import peli21.domain.Ruutu;
 import peli21.gui.Kayttoliittyma;
+import peli21.highscore.Highscorelista;
 import peli21.peli.Peli;
 
 /**
@@ -20,10 +22,10 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here     
         System.out.println("Seisotte nyt paikalla, johon on hyvää vauhtia valmistumassa eeppinen ruudukkopeli.");
 
-        Peli ruudukkopeli = new Peli(20, 10);
+        Peli ruudukkopeli = new Peli("Pelaaja", 20, 10);
         Kayttoliittyma gui = new Kayttoliittyma(ruudukkopeli, 40);
         SwingUtilities.invokeLater(gui);
         while (gui.getPiirtoalusta() == null) {
@@ -33,27 +35,6 @@ public class main {
                 System.out.println("Piirtoalustaa ei ole vielä luotu.");
             }
         }
-
         ruudukkopeli.setPaivitettava(gui.getPiirtoalusta());
-//        
-//        System.out.println("Tehtiin ruudukko mitoilla (5, 4)");
-//        Ruudukko ruudukko1 = new Ruudukko(5, 4);
-//        
-//        ruudukko1.setRuutu(2, 3, new Ruutu(true, true, false, false));
-//        ruudukko1.getRuutu(3, 3).setDown(true);
-//        
-//        Ruutu ruutu1 = ruudukko1.getRuutu(2, 3);
-//        System.out.println("Ruudukon ruutu (2,3): ");
-//        System.out.println("UP: " + ruutu1.isUp());
-//        System.out.println("DOWN: " + ruutu1.isDown());
-//        System.out.println("LEFT: " + ruutu1.isLeft());
-//        System.out.println("RIGHT: " + ruutu1.isRight());
-//        
-//        Ruutu ruutu2 = ruudukko1.getRuutu(3, 3);
-//        System.out.println("Ruudukon ruutu (3,3): ");
-//        System.out.println("UP: " + ruutu2.isUp());
-//        System.out.println("DOWN: " + ruutu2.isDown());
-//        System.out.println("LEFT: " + ruutu2.isLeft());
-//        System.out.println("RIGHT: " + ruutu2.isRight());
     }
 }
--- peli21/src/peli21/peli/Peli.java
+++ peli21/src/peli21/peli/Peli.java
@@ -6,11 +6,13 @@ package peli21.peli;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import javax.swing.Timer;
 import peli21.Suunta;
 //import peli21.domain.Pelihahmo;
 import peli21.domain.Ruudukko;
 import peli21.gui.Paivitettava;
+import peli21.highscore.Highscorelista;
 //import peli21.gui.Piirtoalusta;
 
 /**
@@ -21,6 +23,8 @@ public class Peli implements ActionListener {
     //private int leveys;
     //private int korkeus;
 
+    private Highscorelista highscorelista;
+    private String pelaajanNimi;
     private boolean jatkuu;
     private Timer ajastin;
     private int pistelaskuri;
@@ -28,15 +32,17 @@ public class Peli implements ActionListener {
     private Paivitettava paivitettava;
     //private Pelihahmo pelaaja;
 
-    public Peli(int leveys, int korkeus) {
+    public Peli(String pelaajanNimi, int leveys, int korkeus) {
         this.ajastin = new Timer(1000, null);
         ajastin.setInitialDelay(2000);
         //ajastin.addActionListener(this);
         //this.leveys = leveys;
         //this.korkeus = korkeus;
+        this.pelaajanNimi = pelaajanNimi;
         this.jatkuu = true;
         this.ruudukko = new Ruudukko(leveys, korkeus);
         this.pistelaskuri = 0;
+        this.highscorelista = new Highscorelista(10);
     }
     
     public Ruudukko getRuudukko() {
@@ -59,6 +65,8 @@ public class Peli implements ActionListener {
             case DEATH:
                 System.out.println("DEATH");
                 jatkuu = false;
+                highscorelista.lisaa(pelaajanNimi, pistelaskuri);
+                highscorelista.tulosta();
                 return;
                 
                 
